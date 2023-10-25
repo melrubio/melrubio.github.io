aa4e98e4bcc317cf0ab9968c5d27e751ae1c89f5
melrubio <147679232+melrubio@users.noreply.github.com>
2023-10-24 19:43:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+ cursor: "<span style='color: #a2d2ff;'>|</span>",
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: #a2d2ff;">Apasionada por la tecnlogía, el diseño y la creatividad.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
